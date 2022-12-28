f79d1fe2989b29120b9da33999eaf43f9f700162
jaanonim <mat8mro@gmail.com>
2022-12-28 13:24:35 +0100

    adding custom command decorator

---
+++ utils/command.py
@@ -0,0 +1,38 @@
+import functools
+import logging
+
+from discord.ext import commands
+from discord import Embed, Color
+from utils.errors import DiscordException
+
+
+def command(group, *args, **kwargs):
+    def decorator(func):
+        @group.hybrid_command(*args, **kwargs)
+        @functools.wraps(func)
+        async def wrapper(ctx: commands.Context, *args, **kwargs):
+            value = None
+
+            async def send(*args, **kwargs):
+                await ctx.interaction.response.send_message(*args, **kwargs, ephemeral=True)
+
+            ctx.send = send
+
+            try:
+                value = await func(ctx, *args, **kwargs)
+            except DiscordException as e:
+                await ctx.interaction.response.send_message(
+                    embed=Embed(color=Color.red(), title="Error", description=str(e)),
+                    ephemeral=True)
+            except Exception as e:
+                await ctx.interaction.response.send_message(
+                    embed=Embed(color=Color.red(), title="Error", description="Something went wrong 😥"),
+                    ephemeral=True)
+                logger = logging.getLogger(f'discord.custom.command.{func.__name__}')
+                logger.error(e)
+
+            return value
+
+        return wrapper
+
+    return decorator
---
+++ utils/errors.py
@@ -0,0 +1,2 @@
+class DiscordException(Exception):
+    pass
